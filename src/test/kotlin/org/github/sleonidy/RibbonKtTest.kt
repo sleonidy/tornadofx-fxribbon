fb48abf17b436ef53474f68a7cea4435e9395165
Leonid <sleonidy@gmail.com>
2019-12-19 23:29:32 +0200

    Add more to test app

--- src/test/kotlin/org/github/sleonidy/RibbonKtTest.kt
+++ src/test/kotlin/org/github/sleonidy/RibbonKtTest.kt
@@ -1,41 +1,46 @@
 package org.github.sleonidy
 
+import javafx.geometry.Pos
+import javafx.scene.control.TabPane
+import org.kordamp.ikonli.devicons.Devicons
+import org.kordamp.ikonli.javafx.FontIcon
 import tornadofx.*
 
 
 internal class RibbonTestApp : App(RibbonView::class)
 
 internal class RibbonView : View() {
+    var tabPane by singleAssign<TabPane>()
     override val root = borderpane {
         top {
             ribbon {
                 quickAccessBar {
                     buttons {
-                        quickAccessBarButton("dasdas")
+                        button(graphic = FontIcon.of(Devicons.SAFARI,16))
                     }
                     rightButtons {
-                        quickAccessBarButton("dasdas")
+                        button("dasdas")
                     }
                 }
-                ribbonTab("Blala") {
-                    ribbonGroup("sdadasd") {
-
+                ribbonTab("Tab 1") {
+                    ribbonGroup("Group 1") {
+                        button(graphic = FontIcon.of(Devicons.CODE_BADGE, 32)) {
+                            action {
+                                println("code badge")
+                            }
+                        }
                     }
-                    ribbonGroup("sdadasd") {
+                    ribbonGroup("Group 2") {
                         //                        ribbonItem("sdadas")
                     }
                 }
                 ribbonTab("Tab 2") {
-                    ribbonGroup("sdadasd") {
-                        ribbonItem("sdadas") {
-                            button("DASDasdasd") {
-                                action {
-                                    print("dasdas")
-                                }
-                            }
+                    ribbonGroup("Group 3") {
+                        ribbonItem("sdadas",FontIcon.of(Devicons.CODE,32)) {
+                            combobox<String>(values = listOf("A","B","C")) {  }
                         }
                     }
-                    ribbonGroup("sdadasd") {
+                    ribbonGroup("Group 4") {
                         ribbonItem("sdadas") {
                             button {
 
@@ -45,6 +50,32 @@ internal class RibbonView : View() {
                 }
             }
         }
+        center {
+            tabPane = tabpane {
+
+            }
+        }
     }
 
+    inline fun <reified T : UIComponent> dock() {
+        tabPane.add<T>()
+    }
+
+    override fun onDock() {
+        super.onDock()
+        dock<AnotherView>()
+        dock<AnotherView>()
+    }
+}
+
+class AnotherView : Fragment("my view") {
+    override val root = borderpane {
+        center {
+            button("Center"){
+                action {
+
+                }
+            }
+        }
+    }
 }
