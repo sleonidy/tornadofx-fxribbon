c5c68e78e9a46efb581b9785e5132b5a9c67f65d
Leonid <sleonidy@gmail.com>
2019-12-20 00:29:59 +0200

    Bintray publication setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    id("com.jfrog.bintray") version "1.8.4"
+    `maven-publish`
+    id("name.remal.maven-publish-bintray") version "1.0.161"
 }
 
 group = "org.github.sleonidy"
 version = "0.1.0"
-
+description = "Tornado FX(https://github.com/edvin/tornadofx) binding for FXRibbon(https://github.com/dukke/FXRibbon)"
 repositories {
     mavenCentral()
     jcenter()
@@ -26,4 +29,32 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+tasks {
+
+}
+publishing {
+    publications{
+        create<MavenPublication>("mavenPublication"){
+
+        }
+
+    }
+}
+bintray{
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_PASSWORD")
+    setPublications("mavenPublication")
+    publish = true
+    pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+        repo = "TornadoFX-FXRibbon"
+        name = "TornadoFX-FXRibbon"
+        userOrg = "sleonidy"
+        websiteUrl = "https://github.com/sleonidy/tornadofx-fxribbon"
+        githubRepo = "sleonidy/tornadofx-fxribbon"
+        vcsUrl = "https://github.com/sleonidy/tornadofx-fxribbon"
+        setLabels("kotlin","tornadofx","fxribbon","javafx")
+//        setLicenses("MIT")
+        desc = description
+    })
 }
