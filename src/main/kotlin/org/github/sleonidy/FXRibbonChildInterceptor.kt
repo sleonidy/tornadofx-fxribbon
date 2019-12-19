8aa958fd5d1d0c477fa3c9afe378823d436b3fd4
Leonid <sleonidy@gmail.com>
2019-12-19 23:29:18 +0200

    Fix interceptor for Ribbon

--- src/main/kotlin/org/github/sleonidy/FXRibbonChildInterceptor.kt
+++ src/main/kotlin/org/github/sleonidy/FXRibbonChildInterceptor.kt
@@ -13,7 +13,7 @@ import tornadofx.ChildInterceptor
 class FXRibbonChildInterceptor : ChildInterceptor {
     override fun invoke(parent: EventTarget, node: Node, index: Int?): Boolean {
         return when (parent) {
-            is Ribbon -> if (node is RibbonTab) parent.tabs.add(node) else false
+            is Ribbon -> parent.add(node, index)
             is RibbonTab -> if (node is RibbonGroup) parent.ribbonGroups.add(node) else false
             is RibbonGroup -> if (index == null) parent.nodes.add(node) else {
                 parent.nodes.add(index, node); true
@@ -21,6 +21,18 @@ class FXRibbonChildInterceptor : ChildInterceptor {
             is RibbonItem -> parent.add(node)
             is QuickAccessBar -> parent.add(node)
             else -> false
+        }
+    }
+
+    private fun Ribbon.add(node: Node, index: Int?): Boolean {
+        return when (node) {
+            is RibbonTab -> if (index == null) tabs.add(node) else {
+                tabs.add(index, node); return true
+            }
+            is QuickAccessBar -> {
+                quickAccessBar = node; return true
+            }
+            else -> return false
         }
     }
 
